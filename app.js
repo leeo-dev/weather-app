be48824d1b884855f4f5a2165de11595b428330f
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-09-04 14:21:34 -0300

    fix: prevent GeoCode function throw an error if data returns undefined

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const geocode = require('./utils/geolocation')
 const forecast = require('./utils/forecast')
-// request(urlWeatherStack, (error, response, body) => {
-//   try {
-//     const data = JSON.parse(body)
-//     const weather = data.current
-//     console.log(`${weather.weather_descriptions[0]}. It is currently ${weather.temperature} degress out. It feels like ${weather.feelslike} degress out`)
-//   } catch (error) {
-//     console.log('Unable to connect!')
-//   }
-// })
 
 geocode('Tocantinópolis', (error, data) => {
   if(error) return console.log(error)
   console.log('Data: ', data)
   forecast({latitude: data.latitude, longitude: data.longitude}, (error, data) => {
+    if(error) return console.log(error)
     console.log(data)
   } )
 })
--- utils/geolocation.js
+++ utils/geolocation.js
@@ -3,8 +3,8 @@ const request = require('postman-request')
 const geocode = (adress, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(adress)}.json?access_token=pk.eyJ1IjoibGVlb2RldiIsImEiOiJjbDdtOGx3eWYxcGx0M3dxdWhoNWFieHpzIn0.v5LwiWbiogqzV2OyxlEaMA&limit=1`
   request(url,  (error, response, body) => {
-    const data = JSON.parse(body)
     if(error) return callback('Unable to connect to location services!', undefined)
+    const data = JSON.parse(body)
     if(!data.features.length) return callback('Unable to find location. Try another search', undefined)
     const geolocation = data.features[0]
     const latitude = geolocation.center[1]
