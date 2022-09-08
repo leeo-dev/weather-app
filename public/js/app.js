e730c02802b595b3d60f9e4cf3d38a9c0136c8b7
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-09-07 22:37:30 -0300

    feat: connect front with back end

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,16 @@
-console.log("I'ts working!!!!")
+const form = document.querySelector("#search")
+
+form.addEventListener("submit", async function (event) {
+  event.preventDefault()
+  const data = new FormData(this)
+  const location = Object.fromEntries(data);
+  await getLocation(location)
+  this.reset()
+})
+
+
+async function getLocation (address = "Tocantinópolis") {
+  const data = await fetch(`http://localhost:3000/get/weather?address=${address}`)
+  const json = await data.json()
+  console.log(json)
+}
--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,17 @@ const {Router} = require('express')
 const {getCurrentWeather} = require('./get-weather')
 const router = Router()
 router.get('/', (request, response) => {
-  response.render('index', {
-    title: 'Weather',
-    name: 'Leonardo Albuquerque',
-    author: 'leeodev'
-  })
+    response.redirect('/weather')
 })
 
 router.get('/weather', async (request, response) => {
+  return response.render('index')
+})
+
+router.get('/get/weather', async (request, response) => {
   console.log(request.query)
   if(!request.query?.address) return response.json({error: "You must provide and address!"})
-  const weather = await getCurrentWeather(request.query?.address)
+  // const weather = await getCurrentWeather(request.query?.address)
   return response.json(weather)
 })
 module.exports = router
